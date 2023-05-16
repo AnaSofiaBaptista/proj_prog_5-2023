752c6c4ac29beb6e1789205fd313a7746b2a7ffc
AnaSofiaBaptista <131919863+AnaSofiaBaptista@users.noreply.github.com>
2023-05-16 15:24:00 +0100

    Update Image.cpp

--- Image.cpp
+++ Image.cpp
@@ -7,42 +7,58 @@ namespace prog
     width_ = w;
     height_ = h;
 
-    pixels_ = new Color*[width_];  //guarda na memória a(s) coluna(s) de pixeis
+    // Guarda na memória as colunas de pixeis.
+    pixels_ = new Color*[width_];
 
-    for (int i = 0; i < width_; i++) {    //itera sobre a(s) coluna(s)
+    // Itera sobre as colunas.
+    for (int i = 0; i < width_; i++)
+    {
+      // Guarda na memória as linhas de pixeis.
       pixels_[i] = new Color[height_]; 
 
-      for (int j = 0; j < height_; j++) {    //itera sobre a(s) linha(s)
-        pixels_[i][j] = fill;
+      // Itera sobre as linhas.
+      for (int j = 0; j < height_; j++)
+      {
+        // Preenche a imagem com a cor especificada.
+        pixels_[i][j] = fill; 
       }
     }
   }
 
   Image::~Image()
   {
-    for (int i = 0; i < width_; i++) {
+    for (int i = 0; i < width_; i++)
+    {
+      // Liberta a memória alocada para as linhas de pixeis.
       delete[] pixels_[i];
     }
 
-    delete[] pixels_;                //remove a matriz de pixeis da memória
+    // Liberta a memória alocada para as colunas de pixeis.
+    delete[] pixels_;
   }
 
   int Image::width() const
   {
+    // Retorna a largura da imagem.
     return width_;
   }
 
   int Image::height() const
   {
+    // Retorna a altura da imagem.
     return height_;
   }
 
-  Color& Image::at(int x, int y){
-    return pixels_[x][y];          //serve para modificar cada pixel
+  Color& Image::at(int x, int y)
+  {
+    // Retorna uma referência à cor do pixel na posição (x,y), permitindo que seja modificada.
+    return pixels_[x][y];
   }
 
-  const Color& Image::at(int x, int y) const{
-    return pixels_[x][y];         //apenas para leitura da imagem final
+  const Color& Image::at(int x, int y) const
+  {
+    // Retorna uma referência constante à cor do pixel na posição (x,y), permitindo apenas leitura.
+    return pixels_[x][y];
   }
 
 }
